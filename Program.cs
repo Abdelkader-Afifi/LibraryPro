a4d2cca56f2930e763dc7f77771824e946f70ba1
Abdelkader15 <https://github.com/Abdelkader15>
2025-08-03 13:05:47 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MvcITIProject.Models;
+using MvcITIProject.Repositories;
+using MvcITIProject.UnitOfWorks;
+
+namespace MvcITIProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            //Add Connectionstring and UnitOfWork
+
+            builder.Services.AddDbContext<LibraryContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("dod")).UseLazyLoadingProxies());
+
+
+            builder.Services.AddScoped<UnitOfWork>();
+            builder.Services.AddScoped<AuthorRepository>();
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<LibraryContext>();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.0.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
---
+++ UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,112 @@
+﻿using MvcITIProject.IRepositories;
+using MvcITIProject.Models;
+using MvcITIProject.Repositories;
+
+namespace MvcITIProject.UnitOfWorks
+{
+    public class UnitOfWork
+    {
+        LibraryContext _context;
+        private readonly Dictionary<Type, object> _repositories = new();
+
+        public UnitOfWork(LibraryContext context)
+        {
+            _context = context;
+            
+        }
+        private BookRepository _bookRepo;
+        private IGenericRepositries<Floor> _floors;
+
+        public IGenericRepositries<Floor> Floors
+        {
+            get
+            {
+                return _floors ??= new GenericRepositries<Floor>(_context);
+            }
+        }
+
+
+        private CategoryRepository _categoryRepo;
+
+        public BookRepository Bookrepo
+
+        {
+            get
+            {
+                if (_bookRepo == null)
+                    _bookRepo = new BookRepository(_context);
+
+                return _bookRepo;
+            }
+        }
+        public CategoryRepository CategoryRepo
+        {
+            get
+            {
+                if (_categoryRepo == null)
+                    _categoryRepo = new CategoryRepository(_context);
+                return _categoryRepo;
+            }
+        }
+
+
+        private ShelfRepository _shelfRepo;
+        public ShelfRepository ShelfRepo
+        {
+            get
+            {
+                if (_shelfRepo == null)
+                    _shelfRepo = new ShelfRepository(_context);
+                return _shelfRepo;
+            }
+        }
+
+        public IGenericRepositries<T> Repositries<T>() where T : class
+        {
+            if (_repositories.TryGetValue(typeof(T), out var repo))
+                return (IGenericRepositries<T>)repo;
+
+            var newRepo = new GenericRepositries<T>(_context);
+            _repositories.Add(typeof(T), newRepo);
+            return newRepo;
+        }
+
+        private AuthorRepository _authorRepo;
+
+        private PublisherRepository _publisherRepo;
+        public PublisherRepository Publisherrepo
+        {
+            get
+            {
+                if (_publisherRepo == null)
+                    _publisherRepo = new PublisherRepository(_context);
+                return _publisherRepo;
+            }
+        }
+
+
+        public AuthorRepository authorRepo
+        {
+            get
+            {
+                if (_authorRepo == null)
+                    _authorRepo = new AuthorRepository(_context);
+
+                return _authorRepo;
+            }
+        }
+        public IGenericRepositries<T> Repository<T>() where T : class
+        {
+            if (_repositories.TryGetValue(typeof(T), out var repo))
+                return (IGenericRepositries<T>)repo;
+
+            var newRepo = new GenericRepositries<T>(_context);
+            _repositories.Add(typeof(T), newRepo);
+            return newRepo;
+        }
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
